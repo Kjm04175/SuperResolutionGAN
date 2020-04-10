faad965a5214d5cdb9c080bdd68094b534828b43
KimJeongMin <30641096+Kjm04175@users.noreply.github.com>
2020-04-10 22:21:18 +0900

    Add files via upload

---
+++ SRGAN/SRGanNet.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import Network as net
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+    def forward(self, input):
+
+        return x
